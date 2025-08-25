a3bdf334aa931971d951cd6d31d2253f7f4e4785
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-08-25 16:13:49 +0200

    Extract game logic from script into separate module

---
+++ brain_games/games/even.py
@@ -0,0 +1,17 @@
+import random
+
+MIN = 0
+MAX = 100
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_question():
+    return random.randint(MIN, MAX)
+
+
+def get_correct_answer(question):
+    return "yes" if is_even(question) else "no"
+
+
+def is_even(number):
+    return number % 2 == 0
