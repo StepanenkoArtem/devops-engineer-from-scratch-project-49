fa71be8cda23ecd81a23cbb059c48bb680c3a8f2
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-08-25 16:18:35 +0200

    Init calc game

---
+++ brain_games/games/calc.py
@@ -0,0 +1,15 @@
+import random
+
+MIN = 0
+MAX = 100
+
+DESCRIPTION = "What is the result of the expression?"
+
+
+def get_question():
+    pass
+
+
+def get_correct_answer(question):
+    pass
+
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import calc as game
+from brain_games.scripts.engine import run
+
+
+def main():
+    run(game)
+
+
+if __name__ == "__main__":
+    main()
