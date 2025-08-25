97b781ac6300ff6f575261454beb17b6bf8e7be4
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-08-25 16:37:36 +0200

    implement calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,37 @@
 import random
 
-MIN = 0
-MAX = 100
-
 DESCRIPTION = "What is the result of the expression?"
+
+MIN = 0
+MAX = 10
+OPERATIONS = ["-", "+", "*"]
 
 
 def get_question():
-    pass
+    first_operand = get_operand()
+    second_operand = get_operand()
+    operator = get_operator()
+    return f"{first_operand} {operator} {second_operand}"
 
 
 def get_correct_answer(question):
-    pass
+    first_operand, operator, second_operand = question.split(" ")
+    first_operand = int(first_operand)
+    second_operand = int(second_operand)
 
+    match operator:
+        case "-":
+            return first_operand - second_operand
+        case "+":
+            return first_operand + second_operand
+        case "*":
+            return first_operand * second_operand
+
+
+def get_operand():
+    return random.randint(MIN, MAX)
+
+
+def get_operator():
+    index = random.randint(0, len(OPERATIONS) - 1)
+    return OPERATIONS[index]
